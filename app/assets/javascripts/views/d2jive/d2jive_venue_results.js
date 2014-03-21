8d61e134893d947a05797db212fbf7a31d229091
smspezzano <smspezzano@gmail.com>
2014-03-21 11:42:36 -0700

    cleaned up individiual venue results Backbone view

--- app/assets/javascripts/views/d2jive/d2jive_venue_results.js
+++ app/assets/javascripts/views/d2jive/d2jive_venue_results.js
@@ -1,11 +1,9 @@
-D2Jive.Views.D2JiveLocaleResults = Backbone.View.extend({
+D2Jive.Views.D2JiveVenueResults = Backbone.View.extend({
   id: 'venueResults',
 
   template: HandlebarsTemplates['d2jive/venue_results'],
 
-  events: {
-    'click .venue': 'getshows',
-  },
+  events: {},
 
   render: function(){
     $(this.el).html(this.template(this.model));
@@ -15,21 +13,5 @@ D2Jive.Views.D2JiveLocaleResults = Backbone.View.extend({
   urls: {
     base: "http://api.songkick.com/api/3.0/venues/"
   },
-
-  getShows: function(event){
-  event.preventDefault();
-
-  venueId = $(".venue").val();
-
-  searchURL = this.urls.base + venueId + "/calendar.json?apikey=4ash2icfOuY4R7v5";
-    $.ajax({
-      type: 'get',
-      url: searchURL,
-    }).done(function(data){
-      var view = new D2Jive.Views.D2JiveVenueResults(data);
-      $('#container').html(view.render().el);  
-    });
-
-  }
 
 });
