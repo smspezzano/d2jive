8a5a684254a6f741515bd745ea54958d205fb481
smspezzano <smspezzano@gmail.com>
2014-03-25 09:40:54 -0700

    WIP: trying to integrate Rdio

--- app/assets/javascripts/views/d2jive/d2jive_venue_results.js
+++ app/assets/javascripts/views/d2jive/d2jive_venue_results.js
@@ -13,6 +13,20 @@ D2Jive.Views.D2JiveVenueResults = Backbone.View.extend({
   },
 
   getTracks: function(event){
+
+    var flashvars = {
+      'playbackToken': "GAlNi78J_____zlyYWs5ZG02N2pkaHlhcWsyOWJtYjkyN2xvY2FsaG9zdEbwl7EHvbylWSWFWYMZwfc=",
+      'domain': "localhost",
+      'listener': 'callback_object'    // the global name of the object that will receive callbacks from the SWF
+    };
+
+    var params = {
+      'allowScriptAccess': 'always'
+    };
+
+    swfobject.embedSWF('http://www.rdio.com/api/swf/',// the location of the Rdio Playback API SWF
+      'apiswf', // the ID of the element that will be replaced with the SWF
+      1, 1, '9.0.0', 'expressInstall.swf', flashvars, params, attributes);
     event.preventDefault();
 
     var artistName = $(".artistName").val().replace(/\s+/g, ',');
