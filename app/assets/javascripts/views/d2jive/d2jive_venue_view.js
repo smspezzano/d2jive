d72de6efc0162c52bc7a91773520cdbd2171a627
smspezzano <smspezzano@gmail.com>
2014-03-27 11:56:30 -0700

    added throttle and emepty function to keep model view clean

--- app/assets/javascripts/views/d2jive/d2jive_venue_view.js
+++ app/assets/javascripts/views/d2jive/d2jive_venue_view.js
@@ -1,13 +1,17 @@
 D2Jive.Views.D2JiveVenueView = Backbone.View.extend({
 
-
+  tagName: 'li',
 
   template: HandlebarsTemplates['d2jive/venue_view'],
+  initialize: function(){
+    this.listenTo(this.model, 'change', this.render);
+  },
 
   render: function(){
     var singleVenue = this.model.toJSON();
     this.$el.html(this.template(singleVenue));
     return this; 
   }
+
 
 });
