b4b5fd60092053b82469a51c183a621a59746561
dvmonroe <dvmonroe6@gmail.com>
2014-03-27 23:31:38 -0700

    WIP final commit tonight

--- app/assets/javascripts/views/d2jive/d2jive_locale_results.js
+++ app/assets/javascripts/views/d2jive/d2jive_locale_results.js
@@ -15,25 +15,25 @@ D2Jive.Views.D2JiveLocaleResults = Backbone.View.extend({
 
   render: function(){
     this.$el.empty();
-    if(this.$el.children().length === 1 ){ 
-      this.$el.append(new D2Jive.Views.D2JiveVenueView({model: venue}).render().$el);
-      } else {
     _.each(
       this.collection.models,
       function(venue){
+
         this.$el.append(new D2Jive.Views.D2JiveVenueView({model: venue}).render().$el);
       },
       this
     );
     return this; 
-  }
-},
+  },
+  setVenueId: function(id){
+    this.model = id;
+  },
   getShows: function(event){
     event.preventDefault();
     this.stopListening(this.collection);
-    // this.$el.children().replaceWith(function() {
-    //   return event.currentTarget; 
-    // });
+    this.$el.children().replaceWith(function(){
+      return event.currentTarget.parentElement;
+    });
     var query = window.location.search;
     var location = query.split("=")[1].replace(/\+/g, '%20');
     var venueId = event.currentTarget.id;
