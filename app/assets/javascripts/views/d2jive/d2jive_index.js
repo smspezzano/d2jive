75578edadda5daa9c4ece0b2b205c04dda29d6e4
smspezzano <smspezzano@gmail.com>
2014-04-09 14:40:51 -0700

    took out commented-out code

--- app/assets/javascripts/views/d2jive/d2jive_index.js
+++ app/assets/javascripts/views/d2jive/d2jive_index.js
@@ -14,14 +14,6 @@ D2Jive.Views.D2JiveIndex = Backbone.View.extend({
   },
 
   processKey: function(event) { 
-    // if (event.keyCode != 13){
-    //  //return;
-    // } else {
-    //   this.getCollectionOfVenues(event);
-    // }
-    event.preventDefault();
-    console.log('hit processKey');
-    console.log(event.keyCode);
     if(event.keyCode == 13){
       var location = $("#city").val().replace(/\s+/g, '+');
       Backbone.history.navigate('venues?address='+location+'', {trigger: true});
